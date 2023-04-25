8e9d4ddbb67bb119d48cab6b2d9a13521312acaf
Mohamed M. Abdelgwad <scr2em@gmail.com>
2023-04-25 17:16:49 +0200

    style: formatting the file + adding a comment

--- src/components/codeBlock.ts
+++ src/components/codeBlock.ts
@@ -24,8 +24,11 @@ export const inlineCodeBlock = (code: string, language: string) =>
 
 export const multiLineCodeBlock = (code: string, language: string) => {
   const codeLines = code.split("\n");
-
-  const maxLineChars = codeLines.reduce((acc,curr)=> curr.length > acc ? curr.length : acc, 0);
+  //this line is finding the longest line
+  const maxLineChars = codeLines.reduce(
+    (acc, curr) => (curr.length > acc ? curr.length : acc),
+    0
+  );
   // codicon class align the code to the center, so we must pad it with spaces
   const paddedCode = codeLines
     .map((line) => line.padEnd(maxLineChars + 2))
