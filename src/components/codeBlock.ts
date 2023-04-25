e5f313a1f22ab372e94fed50cc4f9ce04b73f34b
Mohamed M. Abdelgwad <scr2em@gmail.com>
2023-04-25 16:16:14 +0200

    refactor: variable renaming

--- src/components/codeBlock.ts
+++ src/components/codeBlock.ts
@@ -23,11 +23,11 @@ export const inlineCodeBlock = (code: string, language: string) =>
   codeBlock(` ${code} `, language);
 
 export const multiLineCodeBlock = (code: string, language: string) => {
-  const codeSplit = code.split("\n");
+  const codeLines = code.split("\n");
 
-  const maxLineChars = codeSplit.reduce((acc,curr)=> curr.length > acc ? curr.length : acc, 0);
-  // codicon class align the code to the center so we must padd it with spaces
-  const paddedCode = codeSplit
+  const maxLineChars = codeLines.reduce((acc,curr)=> curr.length > acc ? curr.length : acc, 0);
+  // codicon class align the code to the center, so we must pad it with spaces
+  const paddedCode = codeLines
     .map((line) => line.padEnd(maxLineChars + 2))
     .join("\n");
 
