fc5c8efe699d97a04dd20f9daca1ab0feddb517a
Yoav Balasiano <yoavbls@gmail.com>
2023-08-12 20:08:25 +0300

    Use prettier in standalone mode for the browser

--- src/format/prettify.ts
+++ src/format/prettify.ts
@@ -1,7 +1,9 @@
-import { format } from "prettier";
+import parserTypescript from "prettier/parser-typescript";
+import { format } from "prettier/standalone";
 
 export function prettify(text: string) {
   return format(text, {
+    plugins: [parserTypescript],
     parser: "typescript",
     printWidth: 60,
     singleAttributePerLine: false,
