07f11772a7cc80f2822a9fe37c6d2cec873ebd00
Yoav Balasiano <yoavbls@gmail.com>
2024-07-18 01:43:26 +0300

    Use utils from typedash

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,13 @@
 // We re-export modules to be able to replace/change them easily across all the usages
 export { compressToEncodedURIComponent } from "lz-string";
 import dedent from "ts-dedent";
+export { invert, objectKeys } from "typedash";
 
 /**
  * d stands for dedent.
  * it allow us to indent html in template literals without affecting the output
  */
 export const d = dedent;
-
-/** Similar to `Object.keys` but with stricter types */
-export const keys = <const T extends {}>(object: T): Array<keyof T> =>
-  <Array<keyof T>>Object.keys(object);
-
-/** Similar to `Object.values` but with stricter types */
-export const values = <const T extends {}>(object: T): Array<T[keyof T]> =>
-  <Array<T[keyof T]>>Object.values(object);
 
 /**
  * Check if an array contains a string.
@@ -24,24 +17,3 @@ export const has = (
   array: unknown[],
   item: string
 ): item is Extract<(typeof array)[number], string> => array.includes(item);
-
-/**
- * Copied from radash library
- * Returns an object with { [keys]: value }
- * inverted as { [value]: key }
- */
-export const invert = <
-  TKey extends PropertyKey,
-  TValue extends PropertyKey
->(
-  obj: Record<TKey, TValue>
-): Record<TValue, TKey> => {
-  if (!obj) {
-    return {} as Record<TValue, TKey>;
-  }
-  const keys = Object.keys(obj) as TKey[];
-  return keys.reduce((acc, key) => {
-    acc[obj[key]] = key;
-    return acc;
-  }, {} as Record<TValue, TKey>);
-};
