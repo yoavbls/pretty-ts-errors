153e11cb96ac2be538681966a0e3c67f6803862a
kevin ramharak <kevin@ramharak.nl>
2025-10-10 16:28:47 +0200

    split reduce back into filter, map. increase cache size. define constant

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,8 @@ import { has } from "./utils";
 import * as logger from "./logger";
 
 const cache = new Map<string, MarkdownString>();
-const CACHE_SIZE_MAX = 100;
+const CACHE_SIZE_MAX = 1000;
+const supportedDiagnosticSources = ["ts", "ts-plugin", "deno-ts", "js", "glint"];
 
 export function activate(context: ExtensionContext) {
   logger.info('activating pretty-ts-errors');
@@ -31,43 +32,31 @@ export function activate(context: ExtensionContext) {
       e.uris.forEach((uri) => {
         logger.measure(`uri: '${uri.toString()}'`, () => {
           const diagnostics = languages.getDiagnostics(uri);
+          const supportedDiagnostics = diagnostics.filter(diagnostic => diagnostic.source && has(supportedDiagnosticSources, diagnostic.source))
 
-          // use a reduce to prevent multiple iterations over the collection
-          const items = diagnostics
-            .reduce((items, diagnostic) => {
-              if (!diagnostic.source || !has(
-                ["ts", "ts-plugin", "deno-ts", "js", "glint"],
-                diagnostic.source
-              )) {
-                return items;
+          const items = supportedDiagnostics.map(diagnostic => {
+            let formattedMessage = cache.get(diagnostic.message);
+            if (!formattedMessage) {
+              // formatDiagnostic converts message based on LSP Diagnostic type, not VSCode Diagnostic type, so it can be used in other IDEs.
+              // Here we convert VSCode Diagnostic to LSP Diagnostic to make formatDiagnostic recognize it.
+              formattedMessage = new MarkdownString(
+                formatDiagnostic(converter.asDiagnostic(diagnostic), prettify)
+              );
+              formattedMessage.isTrusted = true;
+              formattedMessage.supportHtml = true;
+
+              if (cache.size > CACHE_SIZE_MAX) {
+                const firstCacheKey = cache.keys().next().value!;
+                cache.delete(firstCacheKey);
               }
+              cache.set(diagnostic.message, formattedMessage);
+            }
 
-              let formattedMessage = cache.get(diagnostic.message);
-              if (!formattedMessage) {
-                // formatDiagnostic converts message based on LSP Diagnostic type, not VSCode Diagnostic type, so it can be used in other IDEs.
-                // Here we convert VSCode Diagnostic to LSP Diagnostic to make formatDiagnostic recognize it.
-                formattedMessage = new MarkdownString(
-                  formatDiagnostic(converter.asDiagnostic(diagnostic), prettify)
-                );
-                formattedMessage.isTrusted = true;
-                formattedMessage.supportHtml = true;
-
-                if (cache.size > CACHE_SIZE_MAX) {
-                  const firstCacheKey = cache.keys().next().value!;
-                  cache.delete(firstCacheKey);
-                }
-
-                cache.set(diagnostic.message, formattedMessage);
-              }
-
-              items.push({
-                range: diagnostic.range,
-                contents: [formattedMessage],
-              });
-
-              return items;
-            }, [] as Hover[]);
-
+            return {
+              range: diagnostic.range,
+              contents: [formattedMessage],
+            };
+          });
 
           if (items.length > 0) {
             uriStore.set(uri.fsPath, items);
@@ -103,4 +92,5 @@ export function deactivate() {
   cache.clear();
   logger.trace('clearing uriStore')
   uriStore.clear();
+  logger.dispose();
 }
