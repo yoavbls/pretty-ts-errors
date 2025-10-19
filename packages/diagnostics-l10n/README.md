00817a80f3a02bf5b2a31d238e1d2a5857f58181
kevin ramharak <kevin@ramharak.nl>
2025-10-19 20:56:59 +0200

    initial proposal

---
+++ packages/diagnostics-l10n/README.md
@@ -0,0 +1,70 @@
+# `@pretty-ts-errors/diagnostics-l10n`
+
+## Summary
+
+A tool to generate & distribute lookup tables for diagnostic messages to locales that are supported by the TypeScript compiler.
+
+## Why its needed
+
+One of the requested features of _Pretty TypeScript Errors_ is [support of localization](#144).
+
+The TypeScript compiler supports a flag in the host environment called `--locale`. In the CLI most people have probably never used it, but in editors the TypeScript Language Server can query the editor to ask which locale the user prefers, usually the locale of the editor.
+
+VS Code supports the use of different display languages through [language packs](https://code.visualstudio.com/docs/configure/locales). For the TypeScript Language Features an option exists which defaults to `auto`, which is most likely based on the configured display language of VS Code:
+![A picture of the VS Code TypeScript locale setting](./assets/vscode-ts-locale-option.png)
+
+For Pretty TypeScript Errors currently uses regex magic to parse the diagnostics into parts to know how to format the different parts correctly. When the messages are in a different language everything these regexes stop working.
+The extension needs a way to parse the diagnostic messages language agnostic, to support all the different locales. Preferably one that does not require manually mapping out every single error message in every locale.
+
+## API
+
+What the API should look like and what it should do is still under discussion.
+The current proposal is to provide an API like:
+
+```ts
+// `@pretty-ts-errors/diagnostics-l10n` provides either an export or function to fetch this data structure
+declare const diagnosticMessageLocalesMap: DiagnosticMessageLocalesMap;
+
+// using the spanish locale
+const spanishLocale = "es";
+const errorCode = 2322;
+const diagnosticMessageTemplateInSpanish =
+  diagnosticMessageLocalesMap[spanishLocale][errorCode];
+// -> El tipo '{0}' no se puede asignar al tipo '{1}'.
+
+// with the english locale (also the default locale of the TypeScript compiler)
+const englishLocale = "en";
+const diagnosticMessageTemplateInEnglish =
+  diagnosticMessageLocalesMap[englishLocale][errorCode];
+// -> Type '{0}' is not assignable to type '{1}'
+```
+
+The types are explained in [`src/index.ts`](./src/index.ts)
+
+This should allow the extension to:
+
+- receive the diagnostics
+- split them into parts based on the matching template (fetched based on locale)
+- format the parts into markdown
+- stitch the result back together
+- display it to the user
+
+### Remaining questions
+
+- String encoding and multibyte characters are a thing. Some of the other locale's use non-ASCII characters in the diagnostics. How does the implementation handle this? Does it need to?
+- RTL languages exist, are any of them in the locales TypeScript supports? If so, do we need special handling?
+- How stable are the error codes? There was a big discussion about error codes regarding [`@ts-ignore`](https://github.com/microsoft/TypeScript/issues/19139).
+
+## How does the TypeScript Compiler manage this?
+
+TypeScript itself uses several files to manage its locales.
+
+These files are:
+
+- [`scripts/generateLocalizedDiagnosticMessages.mjs`](https://github.com/microsoft/TypeScript/blob/v5.9.3/scripts/generateLocalizedDiagnosticMessages.mjs)
+- [`scripts/processDiagnosticMessages.mjs`](https://github.com/microsoft/TypeScript/blob/v5.9.3/scripts/processDiagnosticMessages.mjs)
+- [`src/compiler/diagnosticMessages.json`](https://github.com/microsoft/TypeScript/blob/v5.9.3/src/compiler/diagnosticMessages.json)
+- [`src/loc/lcl/<language-code>/diagnosticMessages/diagnosticMessages.generated.json.lcl`](https://github.com/microsoft/TypeScript/tree/v5.9.3/lib)
+- [`lib/<locale>/diagnosticMessages.generated.json`](https://github.com/microsoft/TypeScript/tree/v5.9.3/lib)
+
+These internal tools and files [are used](https://github.com/microsoft/TypeScript/blob/v5.9.3/src/compiler/utilitiesPublic.ts#L685-L758) to swap the diagnostic messages to whatever locale is active at that time.
---
+++ packages/diagnostics-l10n/scripts/create-diagnostics-maps.js
@@ -0,0 +1,194 @@
+// @ts-check
+
+import { fileURLToPath } from "url";
+import { readFile, writeFile } from "fs/promises";
+import * as path from "path";
+
+const tsVersion = "5.9.3";
+const diagnosticMessagesReferenceFileUrl = `https://raw.githubusercontent.com/microsoft/TypeScript/refs/tags/v${tsVersion}/src/compiler/diagnosticMessages.json`;
+
+/**
+ * @returns {Promise<import('../src/').DiagnosticMessagesReference>}
+ */
+async function getDiagnosticMessagesRefererence() {
+  // TODO: cache this result with version tag?
+  const result = await fetch(diagnosticMessagesReferenceFileUrl);
+  return result.json();
+}
+
+/**
+ * Should match the typescript source code at https://github.com/microsoft/TypeScript/blob/v5.9.3/src/compiler/utilitiesPublic.ts#L685
+ *
+ * NOTE: the link should use the version used in {@link tsVersion}
+ */
+const locales = [
+  "cs",
+  "de",
+  "es",
+  "fr",
+  "it",
+  "ja",
+  "ko",
+  "pl",
+  "pt-br",
+  "ru",
+  "tr",
+  "zh-cn",
+  "zh-tw",
+];
+
+/**
+ * @param {string} locale
+ */
+function pathForLocale(locale) {
+  return `${locale}/diagnosticMessages.generated.json`;
+}
+
+/**
+ * should resolve to `node_modules/typescript/lib/`, where `node_modules` is the import path the nodejs runtime uses for this project.
+ * needs to be this complicated because of ESM/CJS is still a mess, and having a monorepo complicates it further
+ */
+const typescriptLibPath = path.dirname(
+  fileURLToPath(import.meta.resolve("typescript"))
+);
+
+async function getAllLocaleDiagnosticMessagesMaps() {
+  return Promise.all(
+    locales
+      .map((locale) => {
+        return {
+          locale,
+          path: path.join(typescriptLibPath, pathForLocale(locale)),
+        };
+      })
+      .map(async ({ locale, path }) => {
+        const text = await readFile(path, { encoding: "utf-8" });
+        /** @type {import('../src/').LocalizedDiagnosticeMessageMap} */
+        const json = JSON.parse(text);
+        return {
+          locale,
+          json,
+        };
+      })
+  );
+}
+
+/**
+ * The locale the diagnostic messages reference file is written into
+ * @type {import('../src/').Locale}
+ */
+const defaultLocale = "en";
+
+/**
+ *
+ * @param {import('../src/').DiagnosticMessagesReference} diagnosticMessageReference
+ * @param {{ locale: string, json: import('../src/').LocalizedDiagnosticeMessageMap }[]} localizedDiagnosticMessagesMaps
+ * @returns {import('../src/').DiagnosticMessageLocalesMap}
+ */
+function mergeToDiagnosticMessageLocalesMap(
+  diagnosticMessageReference,
+  localizedDiagnosticMessagesMaps
+) {
+  /**
+   * @type {import('../src/').DiagnosticMessageLocalesMap}
+   */
+  const result = {};
+  result[defaultLocale] =
+    transformDiagnosticMessagesReferenceToDiagnosticMessageErrorCodeMap(
+      diagnosticMessageReference
+    );
+  localizedDiagnosticMessagesMaps.forEach(({ locale, json }) => {
+    result[locale] =
+      transformLocalizedDiagnosticMessagesToDiagnosticMessageErrorCodeMap(json);
+  });
+  return result;
+}
+
+/**
+ * @param {import('../src/').DiagnosticMessagesReference} diagnosticMessageReference
+ * @returns {import('../src/').DiagnosticMessageErrorCodeMap}
+ */
+function transformDiagnosticMessagesReferenceToDiagnosticMessageErrorCodeMap(
+  diagnosticMessageReference
+) {
+  /**
+   * @type {import('../src/').DiagnosticMessageErrorCodeMap}
+   */
+  const map = {};
+  return Object.entries(diagnosticMessageReference).reduce(
+    (map, [template, { code }]) => {
+      map[code] = template;
+      return map;
+    },
+    map
+  );
+}
+
+/**
+ * @param {import('../src/').LocalizedDiagnosticeMessageMap} localizedDiagnosticMessages
+ * @returns {import('../src/').DiagnosticMessageErrorCodeMap}
+ */
+function transformLocalizedDiagnosticMessagesToDiagnosticMessageErrorCodeMap(
+  localizedDiagnosticMessages
+) {
+  /**
+   * @type {import('../src/').DiagnosticMessageErrorCodeMap}
+   */
+  const map = {};
+  return Object.entries(localizedDiagnosticMessages).reduce(
+    (map, [identifier, template]) => {
+      const parts = identifier.split("_");
+      const code = Number(parts[parts.length - 1]);
+      if (Number.isNaN(code)) {
+        console.warn(
+          `identifier has malformed or NaN error code: ${identifier}`
+        );
+      }
+      if (code < 1000) {
+        console.warn(
+          `identifier did not result into a valid code '${code}': ${identifier}`
+        );
+      }
+      map[code] = template;
+      return map;
+    },
+    map
+  );
+}
+
+/**
+ * ESM version of `__dirname`
+ */
+const dirname = fileURLToPath(new URL(".", import.meta.url));
+
+async function main() {
+  const [diagnosticMessageReference, localizedDiagnosticMessagesMaps] =
+    await Promise.all([
+      getDiagnosticMessagesRefererence(),
+      getAllLocaleDiagnosticMessagesMaps(),
+    ]);
+  const result = mergeToDiagnosticMessageLocalesMap(
+    diagnosticMessageReference,
+    localizedDiagnosticMessagesMaps
+  );
+  const destinationDirectoryPath = path.join(dirname, "..", "src");
+  return Promise.all([
+    writeFile(
+      path.join(destinationDirectoryPath, "diagnosticMessagesMap.json"),
+      JSON.stringify(result, null, 2),
+      { encoding: "utf-8" }
+    ),
+    Object.entries(result).map(([locale, result]) => {
+      return writeFile(
+        path.join(
+          destinationDirectoryPath,
+          `diagnosticMessagesMap.${locale}.json`
+        ),
+        JSON.stringify(result, null, 2),
+        { encoding: "utf-8" }
+      );
+    }),
+  ]);
+}
+
+main();
